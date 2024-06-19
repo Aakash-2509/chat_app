13b83e2f09f805bdac1045499c4775dbd57dbbae
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-06-19 18:51:33 +0530

    chat list page

--- lib/Screens/chatlist/HomePage.dart
+++ lib/Screens/chatlist/HomePage.dart
@@ -1,7 +1,10 @@
 // home_tab.dart
+import 'dart:convert';
+import 'dart:developer';
+import 'package:chat_app/model/chatlist.dart';
+import 'package:chat_app/repo/apis.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../../model/chatlist.dart';
-
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -11,66 +14,75 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
+
+      List<chatuser> list = [];
 // chat_data.dart
-
-
-List<Chat> dummyData = [
-  Chat(
-    name: "John Doe",
-    message: "Hey, how's it going?",
-    time: "15:30",
-    avatarUrl: "https://via.placeholder.com/150",
-  ),
-  Chat(
-    name: "Jane Smith",
-    message: "Are we still meeting tomorrow?",
-    time: "14:30",
-    avatarUrl: "https://via.placeholder.com/150",
-  ),
-  // Add more dummy chats here
-];
-
-
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: dummyData.length,
-      itemBuilder: (context, index) {
-        return Column(
-          children: <Widget>[
-            const Divider(
-              height: 10.0,
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(dummyData[index].avatarUrl),
-                radius: 25.0,
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    dummyData[index].name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    dummyData[index].time,
-                    style: const TextStyle(color: Colors.grey, fontSize: 14.0),
-                  ),
-                ],
-              ),
-              subtitle: Container(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: Text(
-                  dummyData[index].message,
-                  style: const TextStyle(color: Colors.grey, fontSize: 15.0),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
+    return Scaffold(
+      body: StreamBuilder(
+          stream: APIs.firestore.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return const Center(child: CircularProgressIndicator());
+              case ConnectionState.active:
+              case ConnectionState.done:
+                final data = snapshot.data?.docs;
+                //  final list = [];
+
+                 list = data?.map((e) => chatuser.fromJson(e.data())).toList() ?? [];
+                for (var i in data!) {
+                  log('data: ${jsonEncode(i.data())}');
+                  list.add(i.data()['name']);
+                }
+
+                return ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: <Widget>[
+                        const Divider(
+                          height: 10.0,
+                        ),
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(list[index].image),
+                            radius: 25.0,
+                          ),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                list[index].name,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              // Text(
+                              //   list[index].,
+                              //   style: const TextStyle(
+                              //       color: Colors.grey, fontSize: 14.0),
+                              // ),
+                            ], 
+                          ),
+                          subtitle: Container(
+                            padding: const EdgeInsets.only(top: 5.0),
+                            // child: Text(
+                            //   list[index].,
+                            //   style: const TextStyle(
+                            //       color: Colors.grey, fontSize: 15.0),
+                            // ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+            }
+          }),
     );
   }
 }
--- lib/model/chatlist.dart
+++ lib/model/chatlist.dart
@@ -1,9 +1,40 @@
-// chat_model.dart
-class Chat {
-  final String name;
-  final String message;
-  final String time;
-  final String avatarUrl;
+class chatuser {
+  chatuser({
+    required this.image,
+    required this.name,
+    required this.about,
+    required this.isOnline,
+    required this.id,
+    required this.pushToken,
+    required this.email,
+  });
+  late final String image;
+  late final String name;
+  late final String about;
+  late final bool isOnline;
+  late final String id;
+  late final String pushToken;
+  late final String email;
+  
+  chatuser.fromJson(Map<String, dynamic> json){
+    image = json['image'] ?? "";
+    name = json['name']?? "";
+    about = json['about']?? "";
+    isOnline = json['is_online']?? "";
+    id = json['id']?? "";
+    pushToken = json['push_token']?? "";
+    email = json['email']?? "";
+  }
 
-  Chat({required this.name, required this.message, required this.time, required this.avatarUrl});
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['image'] = image;
+    data['name'] = name;
+    data['about'] = about;
+    data['is_online'] = isOnline;
+    data['id'] = id;
+    data['push_token'] = pushToken;
+    data['email'] = email;
+    return data;
+  }
 }
