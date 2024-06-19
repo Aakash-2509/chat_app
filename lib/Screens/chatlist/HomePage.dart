2621b6cd787e1032cb703166b414561605d799ef
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-06-19 14:32:34 +0530

    home page

--- lib/Screens/chatlist/HomePage.dart
+++ lib/Screens/chatlist/HomePage.dart
@@ -1,4 +1,7 @@
+// home_tab.dart
 import 'package:flutter/material.dart';
+import '../../model/chatlist.dart';
+
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -8,8 +11,66 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
+// chat_data.dart
+
+
+List<Chat> dummyData = [
+  Chat(
+    name: "John Doe",
+    message: "Hey, how's it going?",
+    time: "15:30",
+    avatarUrl: "https://via.placeholder.com/150",
+  ),
+  Chat(
+    name: "Jane Smith",
+    message: "Are we still meeting tomorrow?",
+    time: "14:30",
+    avatarUrl: "https://via.placeholder.com/150",
+  ),
+  // Add more dummy chats here
+];
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: <Widget>[
+            const Divider(
+              height: 10.0,
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(dummyData[index].avatarUrl),
+                radius: 25.0,
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    dummyData[index].name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    dummyData[index].time,
+                    style: const TextStyle(color: Colors.grey, fontSize: 14.0),
+                  ),
+                ],
+              ),
+              subtitle: Container(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  dummyData[index].message,
+                  style: const TextStyle(color: Colors.grey, fontSize: 15.0),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
