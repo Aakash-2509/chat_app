61220ded87631ac1ba554e4af758fd70e6926235
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-06-19 13:19:20 +0530

    Bottom nagivation

---
+++ lib/Screens/chatlist/BottomNavigation.dart
@@ -0,0 +1,50 @@
+import 'package:chat_app/Screens/chatlist/HomePage.dart';
+import 'package:chat_app/Screens/chatlist/profileScreen.dart';
+import 'package:flutter/material.dart';
+
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    const HomeTab(),
+    const ProfileTab(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setoo Chat App'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
